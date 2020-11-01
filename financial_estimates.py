2b25c85e21112dafd8cfe7751f88df3c08516022
Eliska Jegrova <jegrovaeliska@gmail.com>
2020-11-01 17:34:36 +0100

    Add functions for month's incomes

--- financial_estimates.py
+++ financial_estimates.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python3
-
 import utils
+
 
 csv_files = utils.get_all_reports()
 df = utils.gather_data(csv_files)
 output_df = utils.create_empty_df(df['date'])
+
+months = utils.get_input_months(df['date'])
+for (month, year) in months:
+    income_sum = utils.income_month(df, year, month)
+    output_df = utils.save_value_to_output(output_df, year, month, income_sum)
+
 print(output_df)
 
-january = utils.data_month_range(df, 2020, 1)
-print(january)
+# TODO create class data
+
+# TODO celkem výdaje v měsící
 
 # TODO součty každého měsíce podle budgetů
 # TODO stav po každém měsíci podle budgetů
-# TODO celkem příjmy v měsíci
-# TODO celkem výdaje v měsící
+
 # TODO rozdíl v měsíci
 # TODO procento neutracených peněz
 # TODO měsíční zůstatek na účtu
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+import datetime
 import glob
 import pandas
+
+
+pandas.set_option('display.max_rows', None)
 
 
 def get_all_reports():
@@ -37,7 +41,7 @@ def data_month_range(data, year, month):
     :return: data for selected month and year
     :rtype: pandas.core.frame.DataFrame
     """
-    return data.loc[f'{year}-{month}-01':f'{year}-{month}-31']
+    return data.loc[f'{year}-{month}']
 
 
 def create_empty_df(date):
@@ -51,14 +55,80 @@ def create_empty_df(date):
     :return: empty output DataFrame
     :rtype: pandas.core.frame.DataFrame
     """
-    month = pandas.DatetimeIndex(date).month
-    year = pandas.DatetimeIndex(date).year
-    unique_months = sorted(set(list(zip(month, year))))
-
+    unique_months = get_input_months(date)
     first_month = unique_months[0][0]
     first_year = unique_months[0][1]
     periods = len(unique_months)
 
     months = [d.strftime('%B-%Y') for d in
               pandas.date_range(start=f'{first_year}-{first_month}', freq='M', periods=periods)]
-    return pandas.DataFrame(0, index=months, columns=['Income', 'Expenses'])
+    return pandas.DataFrame(0.0, index=months, columns=['Income', 'Expenses'])
+
+
+def get_input_months(date):
+    """
+    Get all months and corresponding years from input data
+
+    :param pandas.core.series.Series date: 'date' column in input data
+    :return: month and year tuples
+    :rtype: list of tuples
+    """
+    month = pandas.DatetimeIndex(date).month
+    year = pandas.DatetimeIndex(date).year
+    return sorted(set(list(zip(month, year))))
+
+
+def filter_category_income(data):
+    """
+    Get only the data that has category set to anything from the filter
+
+    :param DataFrame data: input data
+    :return: filtered data
+    :rtype: pandas.core.frame.DataFrame
+    """
+    filter_income = ['Income', 'Checks, coupons', 'Child Support', 'Dues & grants', 'Gifts', 'Interests, dividends',
+                     'Lending, renting', 'Lottery, gambling', 'Refunds (tax, purchase)', 'Rental income', 'Sale',
+                     'Wage, invoices']
+    return data[data['category'].isin(filter_income)]
+
+
+def sum_amount(data):
+    """
+    Get sum of the data in 'ref_currency_amount' column
+
+    :param DataFrame data: input data
+    :return: sum of data amount column
+    :rtype: float
+    """
+    return data['ref_currency_amount'].sum()
+
+
+def income_month(data, year, month):
+    """
+    Get income for specific month
+
+    :param DataFrame data: input data
+    :param int year: selected year
+    :param int month: selected month
+    :return: income value for specific month
+    :rtype: float
+    """
+    data_per_month = data_month_range(data, year, month)
+    income_df = filter_category_income(data_per_month)
+    return sum_amount(income_df)
+
+
+def save_value_to_output(data, year, month, value):
+    """
+    Change Income value in DataFrame for specific month
+
+    :param DataFrame data:
+    :param int year: selected year
+    :param int month: selected month
+    :param value: output value
+    :return: data with changed value
+    :rtype: pandas.core.frame.DataFrame
+    """
+    month_str = datetime.date(year, month, 1).strftime('%B-%Y')
+    data['Income'][month_str] = value
+    return data
